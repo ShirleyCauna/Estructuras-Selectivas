743fed650eb847457a23d4df7a2ea4f7fc4db0af
ShirleyCauna <caunashirley11@gmail.com>
2025-05-29 22:55:03 -0500

    docs: agrega comentarios explicativos en ejercicio 1

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -2,14 +2,22 @@
 using namespace std;
 
 int main(){
-	int N1,N2,N3,P;
+	int N1,N2,N3,P; // Variables para las 3 notas y el promedio
+
+        // Solicitar ingreso de notas al usuario
 	cout<<"Ingrese Nota 1: ";
 	cin>>N1;
+
 	cout<<"Ingrese Nota 2: ";
 	cin>>N2;
+
 	cout<<"Ingrese Nota 3: ";
 	cin>>N3;
+
+        // Calcular el promedio de las 3 notas
 	P=(N1+N2+N3)/3;
+
+        // Verificar si el promedio es mayor o igual a 70
 	if (P >= 70) {
         cout << "El alumno APRUEBA con un promedio de " << P << endl;
     } else {

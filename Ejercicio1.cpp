447b1c864178194c913445cb216c075b241967ad
ShirleyCauna <caunashirley11@gmail.com>
2025-05-31 22:15:45 -0500

    Optimiza ejercicio de promedio: versión más corta del código

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -1,27 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-	int N1,N2,N3,P; // Variables para las 3 notas y el promedio
-
-        // Solicitar ingreso de notas al usuario
-	cout<<"Ingrese Nota 1: ";
-	cin>>N1;
-
-	cout<<"Ingrese Nota 2: ";
-	cin>>N2;
-
-	cout<<"Ingrese Nota 3: ";
-	cin>>N3;
-
-        // Calcular el promedio de las 3 notas
-	P=(N1+N2+N3)/3;
-
-        // Verificar si el promedio es mayor o igual a 70
-	if (P >= 70) {
-        cout << "El alumno APRUEBA con un promedio de " << P << endl;
-    } else {
-        cout << "El alumno REPRUEBA con un promedio de " << P<< endl;
-    }
-	return 0;
+int main() {
+    int nota1, nota2, nota3, promedio;
+    cout << "Ingrese tres notas: ";
+    cin >> nota1 >> nota2 >> nota3;
+    promedio = (nota1 + nota2 + nota3) / 3;
+    if (promedio >= 70)
+        cout << "Aprueba" << endl;
+    else
+        cout << "Reprueba" << endl;
+    return 0;
 }
